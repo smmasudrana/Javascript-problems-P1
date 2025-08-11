33e53bde57cad248b681a184db3d6dc06df0f8fc
smmasudrana <smmasudrana62@gmail.com>
2025-08-11 20:06:48 +0600

    add basic math with js

---
+++ math.js
@@ -0,0 +1,20 @@
+const min =Math.min(46, 64, 947, 37, 11, 98, 66)   //how to get minimum number.
+console.log(min);
+
+const max =Math.max(64, 46, 76, 94, 37, 65, 98)   //how to get maximum number.
+console.log(max);
+
+
+console.log(Math.PI);
+console.log(Math.abs(10-5));
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+console.log(Math.floor(5.95));   //focefully vagsesh bad diye purno songkha count korbe.
+console.log(Math.ceil(5.35));    //forcefully vagsesh jodi .1 o thake tahole sorbocco purnosokha count korbe.
+
+console.log(Math.random());   //ata use korle 0 theke 1 er moddhe jekono akta random songkha dibe.
+console.log(Math.random()*10);   //akhane jodi seshe *10 bosano hoi tahole 10 er moddhe jekono random songkha choose korbe.
+
+
+const rand =Math.round(Math.random()*10)   //jodi math.round er vitore rakhi tahole prottekbar akta kore purno songkha dibe.
+console.log(rand);
